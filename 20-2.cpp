a4eff880149145aba6f1e6670d38fc89428a0df0
LXmaind <110681306+LXmaind@users.noreply.github.com>
2024-07-02 12:21:58 +0300

    Add files via upload

---
+++ 20-2.cpp
@@ -0,0 +1,39 @@
+﻿
+#include <iostream>
+#include <vector>
+#include <iostream>
+#include <vector>
+#include <ctime>
+#include <fstream>
+
+int main() {
+
+    std::srand(std::time(nullptr));
+
+    std::ofstream pr("print.txt", std::ios::trunc);
+
+    if (pr.is_open()) {
+
+        int row = 3;
+        int col = 4;
+
+        std::cout << "enter the number of lines: ";
+        std::cin >> row;
+        std::cout << "enter the number of columns: ";
+        std::cin >> col;
+
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < col; j++)
+            {
+                pr << (rand() % 2) << " ";
+            }
+            pr << "\n";
+        }
+
+    }
+
+    pr.close();
+
+}
+
